a18e2330349b08dd2f229eb0d7a8aec811565b73
Thibault Gautriaud <thibault@mixlr.com>
2018-03-22 15:08:13 +0000

    Ensure Web audio API available on browser

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,67 +18,75 @@
 var writeLog, playSound, playBuffer, setCoverImage;
 
 (function() {
-  var context = new AudioContext();
+  var AudioContext = window.AudioContext // Default
+    || window.webkitAudioContext // Safari and old versions of Chrome
+    || false;
 
-  writeLog = function(msg) {
-    $('.log-container ul').append($('<li/>').text(msg));
-  }
+  if (AudioContext) {
+    var context = new AudioContext();
 
-  playSound = function(sound, reverse, delay) {
-    var request = new XMLHttpRequest();
+    writeLog = function(msg) {
+      $('.log-container ul').append($('<li/>').text(msg));
+    }
 
-    request.open('GET', sound, true);
-    request.responseType = 'arraybuffer';
+    playSound = function(sound, reverse, delay) {
+      var request = new XMLHttpRequest();
 
-    request.addEventListener('load', function() {
-      context.decodeAudioData(request.response, function(buffer) {
-        playBuffer(buffer, reverse, delay);
+      request.open('GET', sound, true);
+      request.responseType = 'arraybuffer';
+
+      request.addEventListener('load', function() {
+        context.decodeAudioData(request.response, function(buffer) {
+          playBuffer(buffer, reverse, delay);
+        });
       });
-    });
 
-    request.send();
-  }
-
-  playBuffer = function(buffer, reverse, delay) {
-    var source, delay, feedback, filter, i;
-
-    source = context.createBufferSource();
-
-    if (delay) {
-      delay = context.createDelay();
-      delay.delayTime.value = 0.3;
-
-      feedback = context.createGain();
-      feedback.gain.value = 0.6;
-
-      filter = context.createBiquadFilter();
-      filter.frequency.value = 1000;
-
-      delay.connect(feedback);
-      feedback.connect(filter);
-      filter.connect(delay);
-
-      source.connect(delay);
-      delay.connect(context.destination);
+      request.send();
     }
 
-    source.connect(context.destination);
+    playBuffer = function(buffer, reverse, delay) {
+      var source, delay, feedback, filter, i;
 
-    if (reverse) {
-      for (i=0; i < buffer.numberOfChannels; i++) {
-        Array.prototype.reverse.call(buffer.getChannelData(i));
+      source = context.createBufferSource();
+
+      if (delay) {
+        delay = context.createDelay();
+        delay.delayTime.value = 0.3;
+
+        feedback = context.createGain();
+        feedback.gain.value = 0.6;
+
+        filter = context.createBiquadFilter();
+        filter.frequency.value = 1000;
+
+        delay.connect(feedback);
+        feedback.connect(filter);
+        filter.connect(delay);
+
+        source.connect(delay);
+        delay.connect(context.destination);
       }
+
+      source.connect(context.destination);
+
+      if (reverse) {
+        for (i=0; i < buffer.numberOfChannels; i++) {
+          Array.prototype.reverse.call(buffer.getChannelData(i));
+        }
+      }
+
+      source.buffer = buffer;
+      source.start();
     }
 
-    source.buffer = buffer;
-    source.start();
-  }
+    setCoverImage = function(image) {
+      var $container = $('.cast-container');
 
-  setCoverImage = function(image) {
-    var $container = $('.cast-container');
-
-    $container.css({
-      'background-image': "url(" + image + ")",
-    });
+      $container.css({
+        'background-image': "url(" + image + ")",
+      });
+    }
+  } else {
+    alert('Sorry but your browser does not support the Web Audio API');
   }
 })();
