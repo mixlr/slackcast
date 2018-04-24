92639b63b9bea96d53b19f2aa11d86a85633ccf9
Thibault Gautriaud <thibault@mixlr.com>
2018-04-24 12:03:06 +0100

    Configure RtClient for heavier team

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,4 +1,16 @@
 Slack.configure do |config|
-  config.token = ENV['SLACK_BOT_API_TOKEN']
+  config.token = ENV.fetch('SLACK_BOT_API_TOKEN')
+
+  config.logger = Logger.new(STDOUT).tap { |l|
+    l.level = ENV.fetch('LOG_LEVEL', Logger::DEBUG)
+  }
 end
 
+Slack::RealTime::Client.configure do |config|
+  # Return timestamp only for latest message object of each channel.
+  config.start_options[:simple_latest] = true
+  # Skip unread counts for each channel.
+  config.start_options[:no_unreads] = true
+  # Increase request timeout to 5 minutes.
+  config.start_options[:request][:timeout] = 300
+end
