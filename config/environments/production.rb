9158c5cdddedffe35bbb66cc53b05d9431bb01c8
Thibault Gautriaud <thibault@mixlr.com>
2019-01-11 14:05:27 +0000

    Remove AR config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 
   # Required for Heroku:
   config.middleware.use CustomActionCable
