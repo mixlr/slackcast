7e0c70a4175291f093d0672061b3b7da4f80686c
Thibault Gautriaud <thibault@mixlr.com>
2019-03-12 10:44:42 +0000

    Change Airbrake configuration

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,19 @@
 Airbrake.configure do |c|
-  c.project_id = ENV['AIRBRAKE_PROJECT_ID']
-  c.project_key = ENV['AIRBRAKE_PROJECT_KEY']
+  c.ignore_environments = %i(development test)
+  c.project_id          = ENV['AIRBRAKE_PROJECT_ID']
+  c.project_key         = ENV['AIRBRAKE_PROJECT_KEY']
+  c.environment         = ENV['RAILS_ENV']
+  c.root_directory      = ENV['HOME']
+  c.performance_stats   = ENV['AIRBRAKE_PERFORMANCE_STATS_ENABLED']
+  c.app_version         = ENV['HEROKU_RELEASE_VERSION']
+  c.versions            = {
+    rails: Rails.version,
+    ruby: RUBY_VERSION,
+    bundler: ENV['BUNDLER_VERSION']
+  }
+
+  at_exit do
+    Airbrake.notify_sync($!) if $!
+  end
 end if Rails.env.production?
+
